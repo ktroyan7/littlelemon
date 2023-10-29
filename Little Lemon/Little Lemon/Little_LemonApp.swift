05b0a3ea14fcd4e6b53181f400ffeb1ca5dc5b13
Kevin <kevin.troyan@gmail.com>
2023-10-28 19:40:26 -0500

    adding xcode files to respository

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Kevin Troyan on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
